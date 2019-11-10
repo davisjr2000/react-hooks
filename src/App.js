ac52045a3263acb3dd4a825c203bba98378891d7
Davis JR <davisjr2000@hotmail.com>
2019-11-10 17:20:52 -0300

    Implement useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [repositories, setRepositories] = useState([]);
+    useEffect(async () => {
+      const response = await fetch('https://api.github.com/users/davisjr2000/repos');
+      const data = await response.json();
+
+      setRepositories(data)
+    }, []);
+
+    useEffect(() => {
+      const filtered = repositories.filter(repo => repo.favorite);
+
+      document.title = `Github Repos (${filtered.length})`
+    }, [repositories]);
+
+    function handleAppRepository() {
+        setRepositories([
+            ... repositories,
+             {id: Math.random, name: "Novo Repo"}
+        ])
+    }
+
+    function handleFavorite(id) {
+     const newRepositories = repositories.map(repo => {
+         return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo
+     })
+
+     setRepositories(newRepositories);
+    }
+
+    return (
+        <>
+            <ul>
+                {repositories.map((repo =>
+                    <li key={repo.id}>
+                        {repo.name}
+                        {repo.favorite && <span>(Favorito)</span>}
+                        <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+                    </li>
+                ))}
+            </ul>
+            <button onClick={handleAppRepository}>
+                Adicionar Repositório
+            </button>
+        </>
+    )
 }
-
-export default App;
